d0bb9184e3b0302ec8c27531ded588f8d21d4b48
agnlin02 <60339710+agnlin02@users.noreply.github.com>
2021-04-20 12:34:28 +0200

    Initial commit

---
+++ Store/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store();
+            store.BeginGame();
+            
+        }
+    }
+}
---
+++ Store/Store.cs
@@ -0,0 +1,33 @@
+using System.Net.Http.Headers;
+using System;
+
+namespace Store
+{
+    public class Store
+    {
+        public void BeginGame() //  < alt + 62       > alt +60         | alt + 124
+        {
+
+            System.Console.WriteLine("This is a store. Here you can buy food. On your 5th purchas youll get a discount");
+            // y[1.0]= 
+            // for (int x = 0; x < 10; x++)
+            // {
+            //     for (int y = 0; y < 10; y++)
+            //     {
+            //         System.Console.Write(".");
+            //     }
+            //     System.Console.WriteLine();
+
+            // }
+            // Console.ReadLine(); 
+            // //två random generators som tillsamans har i som kan antingen vara 1, -1 eller 0. 
+            //Med dessa kan man sedan addera, subdrahera eller ha kvar samma del som. Man har en lista med kordinater. 
+            // console.writeline([x + i, y + 0]); eller console.writline(x + 0, y + i]); 
+
+
+            
+        }
+
+
+    }
+}
